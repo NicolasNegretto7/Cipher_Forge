c90a8970747babd1de71855910e1da05b3595c4d
ivansandoval201919-ai <ivansandoval201919@gmail.com>
2026-07-23 22:54:10 -0300

    feat: initialize database schema with tables for users, collections, images, and favorites

--- Gestion-fotografias/backend/database/schema.sql
+++ Gestion-fotografias/backend/database/schema.sql
@@ -17,8 +17,25 @@ id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 fotografo_id INT NOT NULL, 
 tipo_visibilidad ENUM('privada', 'publica') NOT NULL,
 titulo VARCHAR(40) NOT NULL,
-descripcion VARCHAR(90) NOT NULL,
+descripcion VARCHAR(90),
 creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
 FOREIGN KEY (fotografo_id) REFERENCES usuarios(id) ON DELETE CASCADE
+);
+
+CREATE TABLE imagenes (
+coleccion_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+url_imagen VARCHAR(125) NOT NULL,
+
+FOREIGN KEY (coleccion_id) REFERENCES colecciones(id) ON DELETE CASCADE
+);
+
+CREATE TABLE favoritos (
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+favoritos_id INT NOT NULL,
+url_imagen VARCHAR(125) NOT NULL,
+
+
+FOREIGN KEY (favoritos_id) REFERENCES imagenes (coleccion_id),
+FOREIGN KEY (url_imagen) REFERENCES imagenes (url_imagen)
 );
