30fce51ac93d9e5165eb07f1f364fac85b73b99f
ivansandoval201919-ai <ivansandoval201919@gmail.com>
2026-08-11 17:59:42 -0300

    feat: initialize database schema with tables for users, media, collections, and access management

--- Gestion-fotografias/backend/database/schema.sql
+++ Gestion-fotografias/backend/database/schema.sql
@@ -3,7 +3,8 @@ USE cipher_forge;
 
 CREATE TABLE usuarios(
 id VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT (UUID()),
-nombre_completo VARCHAR(50) NOT NULL,
+nombre VARCHAR(50) NOT NULL,
+apellido VARCHAR(50) NOT NULL,
 email VARCHAR(60) NOT NULL UNIQUE,
 telefono VARCHAR(30),
 email_verificado BOOLEAN DEFAULT FALSE,
