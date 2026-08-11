437ca2c50bb06c398ac45868173b7f602415d597
ivansandoval201919-ai <ivansandoval201919@gmail.com>
2026-08-11 17:32:21 -0300

    feat: initialize database schema with tables for users, collections, multimedia, and associated services

--- Gestion-fotografias/backend/database/schema.sql
+++ Gestion-fotografias/backend/database/schema.sql
@@ -6,6 +6,7 @@ id VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT (UUID()),
 nombre_completo VARCHAR(50) NOT NULL,
 email VARCHAR(60) NOT NULL UNIQUE,
 telefono VARCHAR(30),
+email_verificado BOOLEAN DEFAULT FALSE,
 password_hash VARCHAR(255) NOT NULL,
 rol ENUM('fotografo', 'cliente') NOT NULL 
 
@@ -15,7 +16,7 @@ CREATE TABLE colecciones(
 id VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT (UUID()),
 fotografo_id VARCHAR(36) NOT NULL, 
 tipo_visibilidad ENUM('privada', 'publica') NOT NULL DEFAULT 'privada',
-titulo VARCHAR(40),
+titulo VARCHAR(60),
 descripcion VARCHAR(90),
 creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
@@ -24,8 +25,8 @@ FOREIGN KEY (fotografo_id) REFERENCES usuarios(id) ON DELETE CASCADE
 
 
 CREATE TABLE multimedia (
-id_multimedia VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT (UUID()) ,
-url_original VARCHAR(90) NOT NULL,
+id_multimedia VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT (UUID()),
+ruta_original VARCHAR(255) NOT NULL,
 coleccion_id VARCHAR(36) NOT NULL,
 vista_previa VARCHAR(255) NOT NULL,
 tamanio BIGINT UNSIGNED NOT NULL,
@@ -37,15 +38,69 @@ FOREIGN KEY (coleccion_id) REFERENCES colecciones(id) ON DELETE CASCADE
 
 CREATE TABLE favoritos (
 
-usuarios_id VARCHAR(36) NOT NULL,
-favoritos_id VARCHAR(36) NOT NULL,
+usuario_id VARCHAR(36) NOT NULL,
+favorito_id VARCHAR(36) NOT NULL,
 
-PRIMARY KEY (usuarios_id, favoritos_id),
-FOREIGN KEY (usuarios_id) REFERENCES usuarios(id) ON DELETE CASCADE,
-FOREIGN KEY (favoritos_id) REFERENCES multimedia (id_multimedia) ON DELETE CASCADE
+PRIMARY KEY (usuario_id, favorito_id),
+FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+FOREIGN KEY (favorito_id) REFERENCES multimedia (id_multimedia) ON DELETE CASCADE
 );
 
+CREATE TABLE acceso_colecciones (
+usuario_id VARCHAR(36) NOT NULL,
+coleccion_id VARCHAR(36) NOT NULL,
+permitir_alta_calidad BOOLEAN DEFAULT FALSE,
+permitir_buena_calidad BOOLEAN DEFAULT FALSE,
 
+
+PRIMARY KEY (usuario_id, coleccion_id),
+FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+FOREIGN KEY (coleccion_id) REFERENCES colecciones(id) ON DELETE CASCADE
+);
+
+CREATE TABLE qr_tokens(
+token VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT (UUID()),
+coleccion_id VARCHAR(36) NOT NULL,
+creacion_token TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+tipo ENUM('colaborativo', 'acceso'),
+expiracion DATETIME DEFAULT NULL,
+
+FOREIGN KEY (coleccion_id) REFERENCES colecciones(id) ON DELETE CASCADE
+
+);
+
+CREATE TABLE solicitudes_descarga(
+id_solicitud VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT (UUID()),
+usuario_id VARCHAR(36) NOT NULL,
+coleccion_id VARCHAR(36) NOT NULL,
+solicitud ENUM('pendiente','aprobada', 'rechazada') DEFAULT 'pendiente',
+calidad_descarga ENUM('buena', 'alta') NOT NULL,
+FOREIGN KEY (coleccion_id) REFERENCES colecciones(id) ON DELETE CASCADE,
+FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+);
+
+CREATE TABLE hashtags(
+nombre_hashtags VARCHAR(40) NOT NULL,
+id_hashtags VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT (UUID())
+
+
+);
+
+CREATE TABLE coleccion_hashtags(
+id_hashtags VARCHAR(36) NOT NULL,
+coleccion_id VARCHAR(36) NOT NULL,
+
+PRIMARY KEY (id_hashtags, coleccion_id),
+FOREIGN KEY (id_hashtags) REFERENCES hashtags(id_hashtags) ON DELETE CASCADE,
+FOREIGN KEY (coleccion_id) REFERENCES colecciones(id) ON DELETE CASCADE
+);
+
+CREATE TABLE backups(
+id_backup VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT (UUID()),
+archivo_backup VARCHAR(255) NOT NULL,
+fecha_backup TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+
+);
 
 
 
