b297d4554c9ac93b7006eb34eadbf896f9f2eda9
ivansandoval201919-ai <ivansandoval201919@gmail.com>
2026-08-11 13:59:21 -0300

    feat: initialize database schema with usuarios, colecciones, multimedia, and favoritos tables

--- Gestion-fotografias/backend/database/schema.sql
+++ Gestion-fotografias/backend/database/schema.sql
@@ -2,53 +2,51 @@ CREATE DATABASE IF NOT EXISTS cipher_forge;
 USE cipher_forge;
 
 CREATE TABLE usuarios(
-id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-nombre_completo VARCHAR(60) NOT NULL,
+id VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT (UUID()),
+nombre VARCHAR(25) NOT NULL,
+apellido VARCHAR(25),
 email VARCHAR(60) NOT NULL UNIQUE,
-telefono VARCHAR(30) NOT NULL,
+telefono VARCHAR(30),
 password_hash VARCHAR(255) NOT NULL,
 rol ENUM('fotografo', 'cliente') NOT NULL 
 
 );
 
 CREATE TABLE colecciones(
-id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-fotografo_id INT NOT NULL, 
+id VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT (UUID()),
+fotografo_id VARCHAR(36) NOT NULL DEFAULT (UUID()), 
 tipo_visibilidad ENUM('privada', 'publica') NOT NULL,
-titulo VARCHAR(40) NOT NULL,
+titulo VARCHAR(40),
 descripcion VARCHAR(90),
 creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
 FOREIGN KEY (fotografo_id) REFERENCES usuarios(id) ON DELETE CASCADE
 );
 
-CREATE TABLE imagenes (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    coleccion_id INT NOT NULL,
-    url_imagen VARCHAR(125) NOT NULL,
 
+CREATE TABLE multimedia (
+id_multimedia VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT (UUID()) ,
+url_original VARCHAR(90) NOT NULL,
+coleccion_id VARCHAR(36) NOT NULL,
+vista_previa VARCHAR(255) NOT NULL,
+tamanio BIGINT UNSIGNED NOT NULL,
+es_invitado BOOLEAN NOT NULL DEFAULT FALSE,
+tipo ENUM('videos', 'imagenes') NOT NULL,
 FOREIGN KEY (coleccion_id) REFERENCES colecciones(id) ON DELETE CASCADE
 );
 
+
 CREATE TABLE favoritos (
 
-usuarios_id INT NOT NULL,
-favoritos_id INT NOT NULL,
+usuarios_id VARCHAR(36) NOT NULL DEFAULT (UUID()),
+favoritos_id VARCHAR(36) NOT NULL DEFAULT (UUID()),
 
 PRIMARY KEY (usuarios_id, favoritos_id),
 FOREIGN KEY (usuarios_id) REFERENCES usuarios(id) ON DELETE CASCADE,
-FOREIGN KEY (favoritos_id) REFERENCES imagenes (id) ON DELETE CASCADE
+FOREIGN KEY (favoritos_id) REFERENCES multimedia (id_multimedia) ON DELETE CASCADE
 );
 
-CREATE TABLE acceso_colecciones(
-usuario_id INT NOT NULL,
-coleccion_id INT NOT NULL,
-permitira_alta_calidad BOOLEAN DEFAULT FALSE,
-
-PRIMARY KEY(usuario_id, coleccion_id),
-FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
-FOREIGN KEY (coleccion_id) REFERENCES colecciones(id)
 
 
 
-);
+
