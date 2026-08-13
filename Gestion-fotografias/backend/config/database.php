40840155a59c2b626e4d42e1b0148e0884429f78
ivansandoval201919-ai <ivansandoval201919@gmail.com>
2026-08-13 13:47:39 -0300

    feat: implement Database connection class using PDO and environment variables

--- Gestion-fotografias/backend/config/database.php
+++ Gestion-fotografias/backend/config/database.php
@@ -1,11 +1,32 @@
 <?php
-class database{
-
-    private $host = 'db';
-    private $name = 'cipher_forge';
-    private $user = 'cipher_user';
-    private $password = 'cipher_password';
-    private $port = '3306';
+class Database{
 
 
+
+    public function connect(): PDO{
+    $host = getenv("DB_HOST");
+    $user = getenv("DB_USER");
+    $pass = getenv("DB_PASS");
+    $port = '3306';
+    $name = getenv("DB_NAME");
+    $dsn = "mysql:host=$host;dbname=$name;port=$port;charset=utf8mb4";
+
+
+    $options = [
+        PDO::ATTR_ERRMODE=> PDO::ERRMODE_EXCEPTION,
+    
+    PDO::ATTR_DEFAULT_FETCH_MODE=> PDO::FETCH_ASSOC,
+    
+    PDO::ATTR_EMULATE_PREPARES=> false,
+    ];
+
+try{
+$pdo = new PDO($dsn, $user, $pass, $options);
+
+return $pdo;
+}catch(PDOException $e){
+    error_log("ErrorPDO: " . $e->getMessage());
+    exit("Error de conexión con la base de datos.");
+}
+    }
 }
