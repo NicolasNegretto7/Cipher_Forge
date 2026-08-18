cec6a1d5881e6199f3f729fbe5ace7390cfa6442
ivansandoval201919-ai <ivansandoval201919@gmail.com>
2026-08-18 16:58:56 -0300

    feat: implement user authentication and registration endpoints in index.php

--- Gestion-fotografias/backend/public/index.php
+++ Gestion-fotografias/backend/public/index.php
@@ -23,20 +23,32 @@ if($method === 'POST' && $uri === 'auth/login'){
     $input = json_decode(file_get_contents('php://input'), true);
     
 
-    if(empty($input['email']) || empty($input['hash_password'])){
+    if(empty($input['email']) || empty($input['password_hash'])){
         jsonResponse(['error' => 'Faltan email o contraseña'], 400);
     }
 
     try{
         $pdo = (new Database())->connect();
         $stmt = $pdo->prepare("SELECT * FROM usuario WHERE email = :email");
-        $stmt->execute([''])
-$login = $stmt->fetchAll(PDO::FETCH_ASSOC);
-jsonResponse($login);
+        $stmt->execute([':email' => $input['email']]);
+            $login = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+            if(!$login){
+                jsonResponse(['error' =>'Credenciales incorrectas', 401]);
+            }
+
+
+            if(!password_verify($input['password_hash'], $login['password_hash'])){
+                jsonResponse(['error' => 'Credenciales incorrectas', 401]);
+            }
+                        jsonResponse($login);
+
     }
+    
 
     catch(PDOException $e){
-jsonResponse(['error' => 'Error al loguearte'], 500);
+        jsonResponse(['error' => 'Error al loguearte'], 500);
     }
 }
 
@@ -48,22 +60,34 @@ jsonResponse(['error' => 'Error al loguearte'], 500);
 
 
 
-if($method === 'POST' && $uri === 'auth/login'){
+
+if($method === 'POST' && $uri === 'auth/register'){
     $input = json_decode(file_get_contents('php://input'), true);
     
 
-    if(empty($input['email']) || empty($input['hash_password'])){
+    if(empty($input['email']) || empty($input['password_hash'])){
         jsonResponse(['error' => 'Faltan email o contraseña'], 400);
     }
 
     try{
         $pdo = (new Database())->connect();
-        $stmt = $pdo->prepare("INSERT INTO FROM usuario('nombre_completo','email','password_hash','rol') VALUES(:nombre_completo,:email,:password_hash,:rol)");
-$login = $stmt->fetchAll(PDO::FETCH_ASSOC);
-jsonResponse($login);
+        $stmt = $pdo->prepare("INSERT INTO usuario (nombre_completo, email, telefono, password_hash, email_verificado, rol) VALUES(:nombre_completo, :email, :telefono, :password_hash, :email_verificado, :rol) ");
+        $stmt->execute([':nombre_completo' => $input['nombre_completo'], ':email' => $input['email'], ':telefono' => $input['telefono'], ':password_hash' => $input['password_hash'], ':email_verificado' => $input['email_verificado'], ':rol' => $input['rol']]);
+            $register = $stmt->fetch(PDO::FETCH_ASSOC);
+            jsonResponse($register);
+
+
+            if(!$register){
+                jsonResponse(['error' =>'Error al registrarte', 401]);
+            }
+
+
+            if(password_verify($input['password_hash'], $register['password_hash'])){
+                jsonResponse(['success' => true, 'message' => 'Usuario registrado exitosamente']);
+            }
     }
 
     catch(PDOException $e){
-jsonResponse(['error' => 'Error al loguearte'], 500);
+        jsonResponse(['error' => 'Error al loguearte'], 500);
     }
 }
